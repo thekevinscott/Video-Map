e72ef1ec3ab3282ef72cdecaff3a388ee5119b41
Alan Chatham <alan.chatham@gmail.com>
2012-11-09 23:55:45 -0500

    Adding in comments. //?? starts a comment of something I didn't understand

--- videogetter.js
+++ videogetter.js
@@ -1,4 +1,11 @@
+// This (function($){.....}(jQuery) block makes sure that $ means jQuery only here
 (function($){
+	// Add a videogetter fuction(object?) to videomap
+	//  Videogetter holds a number of public method:
+	//    updateVideo(params), which takes in lat and lon (latitude and longitude coordinates)
+	//       					and updates the currently displayed video and information
+	//    displayInfo(params), which takes in
+	//	  displayVideo(element, id) which takes in an html element and a youtube video ID, and puts the video in the element
 	videomap.videogetter = function() {
 	// videomap.videogetter.updateVideo({lat: 123, lon: Whatevs})
 		var yt_key = 'AI39si5L5vjhQIEHX5djKr_V6TxvyTGEaubSklcGdph3tkNjH7KwrTAcmQ7X4ZqZy-lvdRpMhCYa73ZTqLmu6ZUE9vHk3LjRvg';	
@@ -42,6 +49,8 @@
 			get(params);
 		}
 		var displayInfo = function(params,video) {
+			//?? Where does address come from ??
+			//?? When does callback get called - I don't get it
 			params.callback = function(address) {
 				// If the video doesn't have an address, then display a default message/ get an address from 
 				//  the latitude/longitide values we're searching by
@@ -50,9 +59,10 @@
 				}
 				$(address_target).html(address);
 				
+				// If the video exists, has statistics, and those stats include a view count,
+				//  show it nicely on the display
 				if (video && video['yt$statistics'] && video['yt$statistics']['viewCount']) {
-			
-					//$(views_target).html('Views: ' + video['yt$statistics']['viewCount']);	
+					
 					$(views_target).html('Views: ' + commaSeparateNumber(video['yt$statistics']['viewCount']));	
 				}
 				
@@ -60,32 +70,40 @@
 			videomap.codeAddress(params);
 		}
 		
+		// This function takes in a number, and returns a string with nice commas every 3rd digit
 		function commaSeparateNumber(val){
 			while (/(\d+)(\d{3})/.test(val.toString())){
 			  val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
 			}
 			return val;
 		}
-
+		
+		// displayVideo takes in an HTML element and a youtube video ID and embeds the youtube video in an iframe
+		//  inside that element via jQuery
 		var displayVideo = function(element,id) {
 			var iframe = '<iframe class="youtube-player" type="text/html" src="http://www.youtube.com/embed/'+id+'?autoplay=1" frameborder="0">';
+			// Use jQuery to replace the html inside the given element with the above iframe
 			$(element).html(iframe);
 
 		}
 		
-		var get = function(params) {
-			if (! params.radius) { params.radius = radius_start; }
+		//?? get does something. probably finds a video
+		var get = function(params) {	
+			// If we don't have a radius already, we use our base search radius.
+			//  This gets set in recursive passes, so it'll expand if we don't find any videos
+			if (! params.radius)
+				{ params.radius = radius_start; }
 			console.log('Our search radius: '+params.radius);
+			// Set up the url that we'll use to do our API call with our specific parameters
 			var url = base_url+'&location='+params.lat+','+params.lon+'&location-radius='+params.radius+'km';
 			
+			// Use jQuery ($. === jQuery.) to do a HTTP get request
+			//  gets the url, sends no data ({}), then executes the function upon the reciept of a success code
+			//   to the get request.
 			$.get(url,{},function(data){
-				
 				if (typeof(data)=='object') {
-				
 					if (data.feed) {
-				
 						if (data.feed.entry && data.feed.entry.length) {
-							
 							params.callback(data.feed.entry[0]);
 						} else {
 							
@@ -112,6 +130,7 @@
 			});
 		}
 		return {
+			//?? What's this do?
 			updateVideo : updateVideo
 		}
 	}();
